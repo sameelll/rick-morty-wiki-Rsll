1ad1e952f9d1e4e236d344b594e75e21c7f675b7
Eren Akel <eren.akel.46@gmail.com>
2021-11-21 18:22:40 +0300

    Folder structure (components) is ready.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap';
+import Cards from './components/Cards/Cards';
+import Filters from './components/Filters/Filters';
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-center ubuntu my-4">
-        Rick & Morty <span className="text-primary">Wiki</span></h1>
+        Rick & Morty <span className="text-primary">Wiki</span>
+      </h1>
+      <div className="container">
+        <div className="row">
+          <div className="col-3">
+            <Filters />
+          </div>
+          <div className="col-8">
+            <div className="row">
+              <Cards />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cards = () => {
+    return (
+        <div className="col-4">
+            
+        </div>
+    )
+}
+
+export default Cards
