22fac43fb3be8c4be1dfa40b181f6e5806d1084c
Eren Akel <eren.akel.46@gmail.com>
2021-11-23 19:08:41 +0300

    The episodes page is now ready to use

--- src/Pages/Episode.js
+++ src/Pages/Episode.js
@@ -1,9 +1,65 @@
-import React from 'react'
+// Hooks
+import React, { useState, useEffect } from 'react'
+
+// Components
+import Cards from '../components/Cards/Cards';
+import InputGroup from '../components/Filters/Category/InputGroup';
 
 const Episodes = () => {
+    // States
+    const [id, setId] = useState(1);
+    const [info, setInfo] = useState([]);
+    const [results, setResults] = useState([]);
+        // Destructured data
+    const { air_date, name, episode } = info;
+
+    // Episodes api call
+    const api = `https://rickandmortyapi.com/api/episode/${id}`;
+    
+    useEffect(() => {
+        (async () => {
+            const data = await fetch(api).then(res=>res.json())
+            setInfo(data);
+            
+            const links = await Promise.all(
+                data.characters.map(link => {
+                    return fetch(link).then(res=>res.json());
+                })
+            )
+            setResults(links); 
+        })();
+    }, [api]);
+
     return (
-        <div>
-            The episodes are here
+        <div className="container">
+            <div className="row mb-4">
+                <h1 className="text-center mb-4">
+                    Episode: {" "} 
+                    <span className="text-primary">
+                        {name===""? "Unknown" : name}
+                    </span>
+                    <span className="text-secondary">{" #"}{episode}{" "}</span>                   
+                </h1>
+                <h5 className="text-center">
+                    Air Date: {air_date===""? "Unknown" : air_date}
+                </h5>
+            </div>
+            <div className="row">
+                <div className="col-3">
+                    <h4 className="text-center mb-4">
+                        Pick Episodes
+                    </h4>
+                    <InputGroup 
+                        name="Episode" 
+                        total={51}
+                        setId={setId} />               
+                </div>
+                <div className="col-8">
+                    <div className="row">
+                        <Cards results={results} />
+                    </div>                  
+                </div>
+            </div>           
         </div>
     )
 }
---
+++ src/components/Filters/Category/InputGroup.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const InputGroup = ({ total, name, setId }) => {
+    return (
+        <div class="input-group mb-3">
+            <select
+                onChange={e => {
+                    setId(e.target.value)
+                }} 
+                class="form-select" 
+                id={name}>
+                <option selected>Choose...</option>
+                {[...Array(total).keys()].map(num => {
+                    return(
+                        <option value={num+1}>{name} - {num+1}</option>
+                    )
+                })}
+            </select>
+        </div>
+    )
+}
+
+export default InputGroup
