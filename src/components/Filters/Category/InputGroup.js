e56602c24c60745ac4cb78f86775737c3e8b7e19
Eren Akel <eren.akel.46@gmail.com>
2021-11-23 21:40:32 +0300

    Error handling

--- src/components/Filters/Category/InputGroup.js
+++ src/components/Filters/Category/InputGroup.js
@@ -2,12 +2,12 @@ import React from 'react'
 
 const InputGroup = ({ total, name, setId }) => {
     return (
-        <div Classname="input-group mb-3">
+        <div className="input-group mb-3">
             <select
                 onChange={e => {
                     setId(e.target.value)
                 }} 
-                Classname="form-select" 
+                className="form-select" 
                 id={name}>
                 <option value="1" selected>Choose...</option>
                 {[...Array(total).keys()].map(num => {
